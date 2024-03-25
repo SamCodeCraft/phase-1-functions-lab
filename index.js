1bf05753ad875dd71010ec30fa2f4869d6aa33b9
siyajari samuel <siyajari.samuel@student.moringaschool.com>
2024-03-25 11:04:34 +0300

    Created Functions Lab

--- index.js
+++ index.js
@@ -1,1 +1,79 @@
-// Code your solution in this file!
+// Function to calculate the distance in blocks from headquarters
+function distanceFromHqInBlocks(pickupLocation) {
+  const hqLocation = 42;
+  return Math.abs(pickupLocation - hqLocation); 
+}
+
+// Function to calculate the distance in feet from headquarters
+function distanceFromHqInFeet(pickupLocation) {
+  const blocks = distanceFromHqInBlocks(pickupLocation); 
+  const feetPerBlock = 264; 
+  return blocks * feetPerBlock; 
+}
+
+// Function to calculate the distance in feet between two blocks
+function distanceTravelledInFeet(startBlock, endBlock) {
+  const blocksTravelled = Math.abs(endBlock - startBlock); 
+  const feetPerBlock = 264; 
+  return blocksTravelled * feetPerBlock; 
+}
+
+// Function to calculate the fare price based on the distance travelled
+function calculatesFarePrice(startBlock, endBlock) {
+  const distance = distanceTravelledInFeet(startBlock, endBlock); 
+
+  // Conditions to determine fare price based on the distance
+  if (distance <= 400) {
+    return 0; 
+  } else if (distance > 400 && distance <= 2000) {
+    return (distance - 400) * 0.02; 
+  } else if (distance > 2000 && distance < 2500) {
+    return 25; 
+  } else {
+    return 'cannot travel that far'; 
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
